dd0888390eb433211611dfa341065c76877b2b0e
spencerwong20 <swong140@calpoly.edu>
2021-10-01 15:14:34 -0700

    Removed characters table to fix compile warning

--- src/MyApp.js
+++ src/MyApp.js
@@ -2,25 +2,6 @@ import React, {useState} from 'react';
 import Table from './Table';
 import Form from './Form';
 
-const characters = [
-    {
-      name: 'Charlie',
-      job: 'Janitor',
-    },
-    {
-      name: 'Mac',
-      job: 'Bouncer',
-    },
-    {
-      name: 'Dee',
-      job: 'Aspring actress',
-    },
-    {
-      name: 'Dennis',
-      job: 'Bartender',
-    },
-];
-
 function MyApp() {
   const [characters, setCharacters] = useState([]);
     return (
